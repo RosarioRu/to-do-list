c6e0fe862d1b44a7d73bf74802f8c2c04795b3c3
RosarioRu <mariarruval@gmail.com>
2022-07-15 23:09:42 -0700

    Remove Index() routes.

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -8,12 +8,12 @@ namespace ToDoList.Controllers
   public class ItemsController : Controller
   {
     
-    [HttpGet("/items")]
-    public ActionResult Index()
-    {
-      List<Item> allItems = Item.GetAll();
-      return View(allItems);
-    }
+    // [HttpGet("/items")]
+    // public ActionResult Index()
+    // {
+    //   List<Item> allItems = Item.GetAll();
+    //   return View(allItems);
+    // }
 
     [HttpGet("/items/new")]
     public ActionResult New()
@@ -21,12 +21,12 @@ namespace ToDoList.Controllers
       return View();
     }
 
-    [HttpPost("/items")]
-    public ActionResult Create(string description)
-    {
-      Item myItem = new Item(description);
-      return RedirectToAction("Index");
-    }
+    // [HttpPost("/items")]
+    // public ActionResult Create(string description)
+    // {
+    //   Item myItem = new Item(description);
+    //   return RedirectToAction("Index");
+    // }
 
     [HttpPost("/items/delete")]
     public ActionResult DeleteAll()
