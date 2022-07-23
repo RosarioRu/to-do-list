9ef00cbc4a21e8bf207bd4aded328a84da71a601
RosarioRu <mariarruval@gmail.com>
2022-07-23 12:29:30 -0700

    Add code to startup.cs file that will connect the to_do_list database in MySql.

--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -39,4 +39,9 @@ namespace ToDoList
       });
     }
   }
+
+  public static class DBConfiguration
+  {
+    public static string ConnectionString = "server=localhost;user id=root;password=epicodus5x;port=3306;database=to_do_list;";
+  }
 }
