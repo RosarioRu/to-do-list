18fdb6b3b9f4e0ab4ed1e1fa64ddcfe4878ff4ac
RosarioRu <mariarruval@gmail.com>
2022-07-27 10:55:30 -0700

    Add route to view for displaying Item(s) details.

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -22,5 +22,29 @@ namespace ToDoList.Controllers
       List<Item> model = _db.Items.ToList(); //once in database, it looks for object named "Items" which is the DbSet declaired in ToDoListContext.cs, a property of ->(ToDoListContext : DbContext). LINQ turns this DbSet into a list using the ToList() method
       return View(model); //this list is what is returned and is the model we'll use for the Index view!
     }
+
+    public ActionResult Create() //GET route that was previously, before Entity, called New(). This is where our form for new items lives, I think?
+    {
+      return View();
+    }
+
+    //below Post request takes item as argument (presumably from the form user submits) and adds it to _db.Items, then saves changes to database object (_db) and routes to Index() page/view for Items.
+    [HttpPost]
+    public ActionResult Create(Item item)
+    {
+      _db.Items.Add(item);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    [HttpGet]
+    public ActionResult Details(int id)
+    {
+      
+      Item itemToDisplay = _db.Items.FirstOrDefault(item => item.ItemId == id);
+      return View(itemToDisplay);
+    }
+
+
   }
 }
