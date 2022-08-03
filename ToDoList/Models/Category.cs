2494872697d264b64187d8d68494946433f2eae6
RosarioRu <mariarruval@gmail.com>
2022-08-02 19:28:25 -0700

    Update Item and Category models to include a collection navigation property.

--- ToDoList/Models/Category.cs
+++ ToDoList/Models/Category.cs
@@ -2,15 +2,16 @@ using System.Collections.Generic;
 
 namespace ToDoList.Models
 {
-    public class Category
+  public class Category
     {
         public Category()
         {
-            this.Items = new HashSet<Item>();
+            this.JoinEntities = new HashSet<CategoryItem>();
         }
 
         public int CategoryId { get; set; }
         public string Name { get; set; }
-        public virtual ICollection<Item> Items { get; set; }
+
+        public virtual ICollection<CategoryItem> JoinEntities { get; set; } //this is a 'collection navigation property' as it contains a reference to many related items. This reference allows us to access related Item(s) in controllers and views.
     }
 }
--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,13 +1,21 @@
 using System.Collections.Generic;
 // using MySql.Data.MySqlClient;
 
+using System.Collections.Generic;
+
 namespace ToDoList.Models
 {
-  public class Item
-  {
-    public int ItemId { get; set; }
-    public string Description { get; set; } 
-    public int CategoryId {get; set; }
-    public virtual Category Category {get; set; }
-  }
+    public class Item
+    {
+        public Item()
+        {
+            this.JoinEntities = new HashSet<CategoryItem>();
+            //JointEntities is a COLLECTION NAVIGATION PROPERTY and holds the list of relationships this Item is a part of - this is how we find its related Categories.
+        }
+
+        public int ItemId { get; set; }
+        public string Description { get; set; }
+
+        public virtual ICollection<CategoryItem> JoinEntities { get;} //JointEntities is a COLLECTION NAVIGATION PROPERTY and holds the list of relationships this Item is a part of - this is how we find its related Categories.
+    }
 }
