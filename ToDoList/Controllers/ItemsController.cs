7e9146f5623c2162b4ea74bce1dcad08a2ce242a
RosarioRu <mariarruval@gmail.com>
2022-08-03 18:35:04 -0700

    Refactor to use joint table.

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -1,4 +1,6 @@
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering; //need this for SelectList.
+
+using Microsoft.AspNetCore.Mvc; 
 using ToDoList.Models;
 using System.Collections.Generic;
 using System.Linq; //this allows us to use ToList() method below
@@ -21,66 +23,75 @@ namespace ToDoList.Controllers
     //_db's value is db, which is an instance of DbContext class. It holds reference to te database.
     public ActionResult Index()
     {
-      List<Item> model = _db.Items.ToList(); //once in database, it looks for object named "Items" which is the DbSet declaired in ToDoListContext.cs, a property of ->(ToDoListContext : DbContext). LINQ turns this DbSet into a list using the ToList() method
-      return View(model); //this list is what is returned and is the model we'll use for the Index view!
+      return View(_db.Items.ToList());
     }
 
-    public ActionResult Create() //GET route that was previously, before Entity, called New(). This is where our form for new items lives, I think?
+    [HttpGet]
+    public ActionResult Create()
     {
+      ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
       return View();
-    } 
-
-    //below Post request takes item as argument (presumably from the form user submits) and adds it to _db.Items, then saves changes to database object (_db) and routes to Index() page/view for Items.
-    [HttpPost]
-    public ActionResult Create(Item item)
-    {
-      _db.Items.Add(item);
-      _db.SaveChanges();
-      return RedirectToAction("Index");
     }
 
     [HttpGet]
     public ActionResult Details(int id)
-    {
-      
-      Item itemToDisplay = _db.Items.FirstOrDefault(item => item.ItemId == id);
-      return View(itemToDisplay);
-    }
-
-    [HttpGet] //this will go to a form that will allow editing of item. User will get here by clicking a link to edit a specific item when it that item's details page/view.
-    public ActionResult Edit(int id)
-    {
-      var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+    { //below we first say thisItem is a list of all Item(s) in database, then we 'load' the joinEntities of items by saying .Include the item(s) property called JoinEntities (list of relationships of items and their categories), then load the categories by .ThenInclude the join.Category. This will return list of items with the Categories of the CatgoryItem(s).  Finally we say the one we want is the the item with the ItemId equalling id.... I think?... so we return the item along with associated categories object(s).
+      var thisItem = _db.Items
+        .Include(item => item.JoinEntities)
+        .ThenInclude(join => join.Category)
+        .FirstOrDefault(item => item.ItemId == id);
       return View(thisItem);
     }
 
-    [HttpPost] //POST method will actually edit the item then take user to index of items view.
-    public ActionResult Edit(Item item)
-    {
-      _db.Entry(item).State = EntityState.Modified;
-      _db.SaveChanges();
-      return RedirectToAction("Index");
-    }
-
-    //Vieow to Form to delete individual item below. Link to this is in Details view for each item.
-    [HttpGet]
-    public ActionResult Delete(int id)
-    {
-      var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
-      return View(thisItem);
-    }
-
-    [HttpPost, ActionName("Delete")]
-    public ActionResult DeleteConfirmed(int id)
-    {
-      var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
-      _db.Items.Remove(thisItem);
-      _db.SaveChanges();
-      return RedirectToAction("Index");
-    }
-
-
-
+    
+   
 
   }
 }
+
+//     public ActionResult Create() //GET route that was previously, before Entity, called New(). This is where our form for new items lives, I think?
+//     {
+//       return View();
+//     } 
+
+
+
+
+
+//     [HttpGet] //this will go to a form that will allow editing of item. User will get here by clicking a link to edit a specific item when it that item's details page/view.
+//     public ActionResult Edit(int id)
+//     {
+//       var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+//       return View(thisItem);
+//     }
+
+//     [HttpPost] //POST method will actually edit the item then take user to index of items view.
+//     public ActionResult Edit(Item item)
+//     {
+//       _db.Entry(item).State = EntityState.Modified;
+//       _db.SaveChanges();
+//       return RedirectToAction("Index");
+//     }
+
+//     //Vieow to Form to delete individual item below. Link to this is in Details view for each item.
+//     [HttpGet]
+//     public ActionResult Delete(int id)
+//     {
+//       var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+//       return View(thisItem);
+//     }
+
+//     [HttpPost, ActionName("Delete")]
+//     public ActionResult DeleteConfirmed(int id)
+//     {
+//       var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+//       _db.Items.Remove(thisItem);
+//       _db.SaveChanges();
+//       return RedirectToAction("Index");
+//     }
+
+
+
+
+//   }
+// }
