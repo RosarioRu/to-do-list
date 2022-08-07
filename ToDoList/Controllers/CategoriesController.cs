ad350045ef1282f46f2642951f652d16af354de6
RosarioRu <mariarruval@gmail.com>
2022-08-07 13:07:31 -0700

    Refactor Categories views and controller for many-to-many relationship.

--- ToDoList/Controllers/CategoriesController.cs
+++ ToDoList/Controllers/CategoriesController.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq; //this allows us to use ToList() method below
 using ToDoList.Models;
-
+using Microsoft.EntityFrameworkCore; //need this for using .Include()
 
 
 namespace ToDoList.Controllers
@@ -21,9 +21,8 @@ namespace ToDoList.Controllers
     //_db's value is db, which is an instance of DbContext class. It holds reference to te database.
     public ActionResult Index()
     {
-      List<Category> model = _db.Categories.ToList(); //once in database, it looks for object named "Categories" which is the DbSet declaired in ToDoListContext.cs, a property of ->(ToDoListContext : DbContext). LINQ turns this DbSet into a list using the ToList() method
-      return View(model); //this list is what is returned and is the model we'll use for the Index view!
-    }
+      return View(_db.Categories.ToList()); 
+    } 
 
     [HttpGet]
     public ActionResult Create() // where a form for new categories will be
@@ -42,68 +41,14 @@ namespace ToDoList.Controllers
     [HttpGet]
     public ActionResult Details(int id)
     {
-      Category categoryToDisplay = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
+      Category categoryToDisplay = _db.Categories
+        .Include(category => category.JoinEntities)//loads all CategoryItem(s) this category is a part of!
+        .ThenInclude(join=> join.Item) //loads the .Item property of each of those CategoryItem(s)
+        .FirstOrDefault(category => category.CategoryId == id); //finally loads the CategoryItem's CategoryId(s) for all the CategoryItem(s) matching... so.. will return the item passed into this controller as a parameter but also include it's Item(s)?...
       return View(categoryToDisplay);
     }
+
 
   }
 }
 
-// using System.Collections.Generic;
-// using System;
-// using Microsoft.AspNetCore.Mvc;
-// using ToDoList.Models;
-
-// namespace ToDoList.Controllers
-// {
-//   public class CategoriesController : Controller
-//   {
-
-//     [HttpGet("/categories")]
-//     public ActionResult Index()
-//     {
-//       List<Category> allCategories = Category.GetAll();
-//       return View(allCategories);
-//     }
-
-//     [HttpGet("/categories/new")]
-//     public ActionResult New()
-//     {
-//       return View();
-//     }
-
-//     [HttpPost("/categories")]
-//     public ActionResult Create(string categoryName)
-//     {
-//       Category newCategory = new Category(categoryName);
-//       return RedirectToAction("Index");
-//     }
-
-//     [HttpGet("/categories/{id}")]
-//     public ActionResult Show(int id)
-//     {
-//       Dictionary<string, object> model = new Dictionary<string, object>();
-//       Category selectedCategory = Category.Find(id);
-//       List<Item> categoryItems = selectedCategory.Items;
-//       model.Add("category", selectedCategory);
-//       model.Add("items", categoryItems);
-//       return View(model);
-//     }
-
-//     // This one creates new Items within a given Category, not new Categories:
-//     [HttpPost("/categories/{categoryId}/items")]
-//     public ActionResult Create(int categoryId, string itemDescription)
-//     {
-//       Dictionary<string, object> model = new Dictionary<string, object>();
-//       Category foundCategory = Category.Find(categoryId);
-//       Item newItem = new Item(itemDescription);
-//       newItem.Save();    // New code
-//       foundCategory.AddItem(newItem);
-//       List<Item> categoryItems = foundCategory.Items;
-//       model.Add("items", categoryItems);
-//       model.Add("category", foundCategory);
-//       return View("Show", model);
-//     }
-
-//   }
-// }
