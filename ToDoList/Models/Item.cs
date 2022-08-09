c9438154b459ff8bda5afccd811eba6cdf77035a
RosarioRu <mariarruval@gmail.com>
2022-08-08 20:26:02 -0700

    Add bool property to Item class called 'Completed' set to default of false.

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -12,6 +12,7 @@ namespace ToDoList.Models
 
         public int ItemId { get; set; }
         public string Description { get; set; }
+        public bool Completed { get; set; } = false;
 
         public virtual ICollection<CategoryItem> JoinEntities { get;} //JointEntities is a COLLECTION NAVIGATION PROPERTY and holds the list of relationships this Item is a part of - this is how we find its related Categories.
     }
